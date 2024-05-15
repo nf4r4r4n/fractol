330f2960c983cbf33dd1594d8b157c24a4d83902
Nomenatsilavina Kevin Fararanoharison <nfararan@c1r8s11.42antananarivo.mg>
2024-05-15 16:13:30 +0300

    reset

--- src/ft_tools.c
+++ src/ft_tools.c
@@ -6,7 +6,7 @@
 /*   By: nfararan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 16:53:27 by nfararan          #+#    #+#             */
-/*   Updated: 2024/05/15 10:30:14 by nfararan         ###   ########.fr       */
+/*   Updated: 2024/05/15 16:12:47 by nfararan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,11 @@ void	ft_put_pix(t_fract *f, int x, int y, int color)
 {
 	char	*pix;
 
-	if (x >= 0 && x < W && y >= 0 && y < H)
-	{
-		pix = f->addr + (y * f->ll + x * (f->bpp / 8));
-		*(int *)pix = color;
-	}
+	pix = f->addr + (y * f->ll + x * (f->bpp / 8));
+	*(unsigned int*)pix = color;
 }
 
 void	ft_put_img_to_win(t_fract *f, int x, int y)
 {
-	mlx_put_image_to_window(f->mlx, (void *)f->win, f->img, x, y);
+	mlx_put_image_to_window(f->mlx, f->win, f->img, x, y);
 }
