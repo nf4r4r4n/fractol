24ba98f510a3cf71decee3f18a9ec15573d0d6c3
Nomenatsilavina Kevin Fararanoharison <nfararan@c1r8s11.42antananarivo.mg>
2024-05-15 16:26:11 +0300

    do not make render in mlx loop

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nfararan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 13:59:45 by nfararan          #+#    #+#             */
-/*   Updated: 2024/05/15 12:03:15 by nfararan         ###   ########.fr       */
+/*   Updated: 2024/05/15 16:22:32 by nfararan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	main(int argc, char **argv)
 		ft_exit("Fractol malloc", 1, NULL);
 	ft_handle_args(argc, argv, fract);
 	ft_init_fractol(fract);
+	ft_render(fract, argv[1][0]);
 	mlx_loop_hook(fract->mlx, &ft_loop_hook, fract);
 	mlx_key_hook(fract->win, &ft_key_hook, fract);
 	mlx_hook(fract->win, 17, 0L, &ft_close, fract);
